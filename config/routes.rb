36095b359cb5ea63bfdf197e3dc2d25f18a6ba34
ariannakosten <arianna.kosten@gmail.com>
2019-10-17 01:27:10 -0700

    add omniauth controller to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :students
+  devise_for :students controllers: { omniauth_callbacks: 'omniauth' }
   resources :klasses
   resources :instructors
   resources :students
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
